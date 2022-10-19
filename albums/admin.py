3bde7fb73358860b998b5c95246dbcc7b10137d3
Ismail Akram <Ismailakram2000@gmail.com>
2022-10-19 20:56:11 +0200

    updated album admin

--- albums/admin.py
+++ albums/admin.py
@@ -9,9 +9,9 @@ class AlbumForm(forms.ModelForm):
     class Meta:
         help_texts = {'approved': "Approve the album if its name is not explicit",}
 
-@admin.register(Albums)
-
 class AlbumAdmin(admin.ModelAdmin):
     form = AlbumForm
     list_display=['artist','name','creation_date','release_date','cost','approved',]
     readonly_fields=['creation_date']
+
+admin.site.register(Albums, AlbumAdmin)
