cf982975b64cdcdf6212413aa00ff174108c6b91
Ismail Akram <Ismailakram2000@gmail.com>
2022-10-29 01:22:18 +0200

    Task4 done

--- artists/admin.py
+++ artists/admin.py
@@ -3,7 +3,6 @@ from .models import Artist
 from albums.admin import AlbumsInline
 
 class ArtistAdmin(admin.ModelAdmin):
-    inlines = [AlbumsInline]
     list_display = ["Stage_name", "Social_link", "approved_albums"]
 
 admin.site.register(Artist, ArtistAdmin)
